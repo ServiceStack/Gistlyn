816aa70edb7d81fe7f68d24ec2e20a90f8906066
Demis Bellot <demis.bellot@gmail.com>
2016-06-29 17:32:40 -0400

    Add JsonViewer component

---
+++ src/Gistlyn/src/json-viewer.js
@@ -0,0 +1,73 @@
+System.register(['react'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __extends = (this && this.__extends) || function (d, b) {
+        for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+    var React;
+    var JsonViewer, show, keyFmt, uniqueKeys, valueFmt, num, date, pad, dmft, str, obj, arr, val;
+    return {
+        setters:[
+            function (React_1) {
+                React = React_1;
+            }],
+        execute: function() {
+            JsonViewer = (function (_super) {
+                __extends(JsonViewer, _super);
+                function JsonViewer() {
+                    _super.apply(this, arguments);
+                }
+                JsonViewer.prototype.render = function () {
+                    var value = this.props.value || JSON.parse(this.props.json);
+                    return (React.createElement("div", {className: "jsonviewer"}, val(value)));
+                };
+                return JsonViewer;
+            }(React.Component));
+            exports_1("JsonViewer", JsonViewer);
+            show = function (k) { return typeof k !== "string" || k.substr(0, 2) !== "__"; };
+            keyFmt = function (t) { return t; };
+            uniqueKeys = function (m) {
+                var h = {};
+                for (var i = 0, len = m.length; i < len; i++) {
+                    for (var k in m[i]) {
+                        if (show(k))
+                            h[k] = k;
+                    }
+                }
+                return h;
+            };
+            valueFmt = function (k, v, vFmt) { return vFmt; };
+            num = function (m) { return m; };
+            date = function (s) { return new Date(parseFloat(/Date\(([^)]+)\)/.exec(s)[1])); };
+            pad = function (d) { return d < 10 ? '0' + d : d; };
+            dmft = function (d) { return d.getFullYear() + '/' + pad(d.getMonth() + 1) + '/' + pad(d.getDate()); };
+            str = function (m) { return m.substr(0, 6) === '/Date(' ? dmft(date(m)) : m; };
+            obj = function (m) {
+                return (React.createElement("dl", null, Object.keys(m).filter(show).map(function (k) { return ([React.createElement("dt", {className: "ib"}, keyFmt(k)), React.createElement("dd", null, valueFmt(k, m[k], val(m[k])))]); })));
+            };
+            arr = function (m) {
+                if (typeof m[0] == 'string' || typeof m[0] == 'number')
+                    return React.createElement("span", null, m.join(', '));
+                var h = uniqueKeys(m);
+                return (React.createElement("table", null, React.createElement("caption", null), React.createElement("thead", null, React.createElement("tr", null, Object.keys(h).map(function (k) { return (React.createElement("th", null, React.createElement("b", null), keyFmt(k))); }))), React.createElement("tbody", null, m.map(function (row) { return (React.createElement("tr", null, Object.keys(h).filter(show).map(function (k) { return React.createElement("td", null, valueFmt(k, row[k], val(row[k]))); }))); }))));
+            };
+            val = function (m, valueFn) {
+                if (valueFn === void 0) { valueFn = null; }
+                if (valueFn)
+                    valueFmt = valueFn;
+                if (m == null)
+                    return "";
+                if (typeof m == "number")
+                    return num(m);
+                if (typeof m == "string")
+                    return str(m);
+                if (typeof m == "boolean")
+                    return m ? "true" : "false";
+                return m.length ? arr(m) : obj(m);
+            };
+        }
+    }
+});
+//# sourceMappingURL=json-viewer.js.map
---
+++ src/Gistlyn/src/json-viewer.tsx
@@ -0,0 +1,78 @@
+﻿import * as React from 'react';
+
+export interface IPrettyPrintProps {
+    value?: any;
+    json?: string;
+}
+
+export class JsonViewer extends React.Component<IPrettyPrintProps, any> {
+    render() {
+        const value = this.props.value || JSON.parse(this.props.json);
+        return (<div className="jsonviewer">{val(value)}</div>);
+    }
+}
+
+const show = (k) => typeof k !== "string" || k.substr(0, 2) !== "__";
+const keyFmt = t => t;
+const uniqueKeys = (m: any[]): any => {
+    var h = {};
+    for (var i = 0, len = m.length; i < len; i++) {
+        for (let k in m[i]) {
+            if (show(k))
+                h[k] = k;
+        }
+    }
+    return h;
+};
+
+var valueFmt = (k, v, vFmt) => vFmt;
+
+const num = (m) => m;
+const date = (s) => new Date(parseFloat(/Date\(([^)]+)\)/.exec(s)[1]));
+const pad = (d) => d < 10 ? '0' + d : d;
+const dmft = (d) => d.getFullYear() + '/' + pad(d.getMonth() + 1) + '/' + pad(d.getDate());
+var str = (m) => m.substr(0, 6) === '/Date(' ? dmft(date(m)) : m;
+
+const obj = (m:any) => {
+    return (
+        <dl>
+            {Object.keys(m).filter(show).map(k => (
+                [<dt className="ib">{keyFmt(k)}</dt>,<dd>{valueFmt(k, m[k], val(m[k]))}</dd>]
+            ))}
+        </dl>
+    );
+}
+
+const arr = (m: any[]) => {
+    if (typeof m[0] == 'string' || typeof m[0] == 'number')
+        return <span>{m.join(', ') }</span>;
+
+    var h = uniqueKeys(m);
+    return (
+        <table>
+            <caption></caption>
+            <thead>
+                <tr>
+                    {Object.keys(h).map(k => (<th><b></b>{keyFmt(k) }</th>)) }
+                </tr>
+            </thead>
+            <tbody>
+                {m.map(row => (
+                    <tr>
+                        {Object.keys(h).filter(show).map(k => <td>{valueFmt(k, row[k], val(row[k])) }</td>) }
+                    </tr>
+                )) }
+            </tbody>
+        </table>
+    );
+}
+
+const val = (m: any, valueFn: (k: string, v: any, vFmt: string) => string = null) => {
+    if (valueFn)
+        valueFmt = valueFn;
+    if (m == null) return "";
+    if (typeof m == "number") return num(m);
+    if (typeof m == "string") return str(m);
+    if (typeof m == "boolean") return m ? "true" : "false";
+    return m.length ? arr(m) : obj(m);
+}
