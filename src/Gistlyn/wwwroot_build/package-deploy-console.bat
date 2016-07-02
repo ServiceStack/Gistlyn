2c44b57e98102281efcacb94663cb92cd7a79f82
Demis Bellot <demis.bellot@gmail.com>
2016-07-01 20:06:48 -0400

    check in updated ILMerge.exe

--- src/Gistlyn/wwwroot_build/package-deploy-console.bat
+++ src/Gistlyn/wwwroot_build/package-deploy-console.bat
@@ -2,16 +2,18 @@
 SET STAGING=staging-console
 
 IF EXIST %STAGING%\ (
-RMDIR /S /Q .\%STAGING%
+REM RMDIR /S /Q .\%STAGING%
 ) ELSE IF EXIST %STAGING% (
-DEL /s %STAGING%
+REM DEL /s %STAGING%
 )
 
-MD %STAGING%
+REM MD %STAGING%
 
 SET TOOLS=.\tools
 SET OUTPUTNAME=Gistlyn-console.exe
 SET ILMERGE=%TOOLS%\ILMerge.exe
+REM SET ILMERGE=%TOOLS%\ILRepack.exe
+
 SET RELEASE=..\..\Gistlyn.AppConsole\bin\Release
 SET INPUT=%RELEASE%\Gistlyn.AppConsole.exe
 SET INPUT=%INPUT% %RELEASE%\Gistlyn.Resources.dll
@@ -25,8 +27,18 @@ SET INPUT=%INPUT% %RELEASE%\ServiceStack.Interfaces.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.Server.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.OrmLite.dll
 SET INPUT=%INPUT% %RELEASE%\ServiceStack.Redis.dll
+SET INPUT=%INPUT% %RELEASE%\Microsoft.CodeAnalysis.CSharp.dll
+SET INPUT=%INPUT% %RELEASE%\Microsoft.CodeAnalysis.CSharp.Scripting.dll
+SET INPUT=%INPUT% %RELEASE%\Microsoft.CodeAnalysis.dll
+SET INPUT=%INPUT% %RELEASE%\Microsoft.CodeAnalysis.Scripting.dll
+SET INPUT=%INPUT% %RELEASE%\NuGet.Core.dll
+SET INPUT=%INPUT% %RELEASE%\System.Collections.Immutable.dll
+SET INPUT=%INPUT% %RELEASE%\System.IO.FileSystem.dll
+SET INPUT=%INPUT% %RELEASE%\System.IO.FileSystem.Primitives.dll
+SET INPUT=%INPUT% %RELEASE%\System.Reflection.Metadata.dll
 
-%ILMERGE% /target:exe /targetplatform:v4,"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.5" /out:%STAGING%\%OUTPUTNAME% /ndebug %INPUT% 
+%ILMERGE% /target:exe /targetplatform:4 /lib:"C:\Program Files (x86)\Reference Assemblies\Microsoft\Framework\.NETFramework\v4.6" /out:%STAGING%\%OUTPUTNAME% /ndebug /copyattrs %INPUT% 
+
 
 IF NOT EXIST apps (
 MD apps
