ebd894df95634e7fda737183b104e54e84f86700
Demis Bellot <demis.bellot@gmail.com>
2016-07-01 20:05:30 -0400

    tweak form

--- src/Gistlyn.AppWinForms/FormMain.Designer.cs
+++ src/Gistlyn.AppWinForms/FormMain.Designer.cs
@@ -35,12 +35,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(426, 403);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormMain";
             this.ShowIcon = false;
+            this.Text = "Gistlyn";
             this.ResumeLayout(false);
 
         }
