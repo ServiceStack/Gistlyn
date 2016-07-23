d17b2c635d1357c405a13eb223ceb90588eebf5b
Demis Bellot <demis.bellot@gmail.com>
2016-07-22 21:49:18 -0400

    Add support for partial AppHost.secrets.cs to override built-in appSettings for live

--- src/Gistlyn/AppHost.cs
+++ src/Gistlyn/AppHost.cs
@@ -1,25 +1,25 @@
-﻿using System.IO;
-using Funq;
+﻿using Funq;
 using Gistlyn.ServiceInterface;
 using ServiceStack;
 using ServiceStack.Configuration;
 
 namespace Gistlyn
 {
-    public class AppHost : AppHostBase
+    public partial class AppHost : AppHostBase
     {
         public AppHost()
             : base("Gistlyn", typeof(RunScriptService).Assembly)
         {
-            var customSettings = new FileInfo(@"~/appsettings.txt".MapHostAbsolutePath());
-            AppSettings = customSettings.Exists
-                ? (IAppSettings)new TextFileSettings(customSettings.FullName)
-                : new AppSettings();
+            var defaultSettings = SharedAppHostConfig.GetMemoryAppSettings();
+            OverrideAppSettings(defaultSettings);
+            AppSettings = defaultSettings;
         }
 
         public override void Configure(Container container)
         {
             SharedAppHostConfig.Configure(this, "~/App_Data/packages".MapHostAbsolutePath());
         }
+
+        static partial void OverrideAppSettings(DictionarySettings appSettings);
     }
 }
