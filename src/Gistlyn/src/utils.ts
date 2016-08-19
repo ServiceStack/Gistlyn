7d49222ebe17362c055c431baec1413b4237af44
Demis Bellot <demis.bellot@gmail.com>
2016-08-19 01:05:17 -0400

    Add TODO gists on non-fork list

--- src/Gistlyn/src/utils.ts
+++ src/Gistlyn/src/utils.ts
@@ -19,10 +19,14 @@ export const GistTemplates = {
     SnapshotsCollection: "1576fda8eea87abbe94fa8051b4fed34",
     HomeCollection: "2cc6b5db6afd3ccb0d0149e55fdb3a6a",
     DownloadCollection: "74d7b0467a197f678bb4220b2c301ac3",
+    RedisTodo: "54e452bb1e86e132068a595d7e72d1a6",
+    OrmLiteTodo: "0cd558e817f28f77b974c44c3e12ff6f",
+    PocoDynamoTodo: "d36339c55be6a43942a60c1eaf687bfd",
     Gists: ["52c37e37b51a0ec92810477be34695ae", "492e199fa3ec5394ef0bc1aedd3240c7", "854ec4df3502ecdfe9ca24d4745e484f",
         "2dbd4ccff70851ce8ae55678f4f15d0a", "363605c3c121784ebababac4a03e8910",
         "457a7035675513ba1365195658a5d792", "1576fda8eea87abbe94fa8051b4fed34",
-        "2cc6b5db6afd3ccb0d0149e55fdb3a6a", "74d7b0467a197f678bb4220b2c301ac3"]
+        "2cc6b5db6afd3ccb0d0149e55fdb3a6a", "74d7b0467a197f678bb4220b2c301ac3",
+        "54e452bb1e86e132068a595d7e72d1a6", "0cd558e817f28f77b974c44c3e12ff6f", "d36339c55be6a43942a60c1eaf687bfd"]
 };
 
 export const FileNames = {
